fcddaf8ad15e27b5cf9e73d281384a2984151886
Anthony Printup <92564080+anthonyprintup@users.noreply.github.com>
2023-02-14 23:01:04 +0100

    core: Added a POC for drawing on a canvas that's attached to the board.

--- src/main.py
+++ src/main.py
@@ -12,7 +12,8 @@ from typing import Any, AsyncIterator, Pattern, TypeVar
 
 import chess
 from chess.engine import Limit as ChessEngineLimit, UciProtocol as ChessEngine, popen_uci as open_chess_engine
-from playwright.async_api import Browser, BrowserContext, Page, Playwright, WebSocket, async_playwright
+from playwright.async_api import Browser, BrowserContext, JSHandle, Locator, Page, Playwright, WebSocket, \
+    async_playwright
 
 BINARIES_PATH: Path = Path.cwd() / "binaries"
 STOCKFISH_PATH: Path = BINARIES_PATH / "stockfish"
@@ -41,9 +42,10 @@ class Round:
     chess_engine: ChessEngine
     chess_engine_limits: ChessEngineLimit = field(default_factory=ChessEngineLimit)
     _takeback_offer_origin: str = ""
+    shadow_root: JSHandle = field(init=False)
 
     @staticmethod
-    async def create(move_data: list[dict]) -> Round:
+    async def create(page: Page, move_data: list[dict]) -> Round:
         # Setup the chess board
         chess_board: chess.Board = chess.Board(fen=move_data[0]["fen"])
         for move in move_data[1:]:
@@ -55,7 +57,10 @@ class Round:
         # Spawn an engine instance and configure the engine
         transport, engine = await open_chess_engine(str(STOCKFISH_ENGINE_PATH))
         await engine.configure(options=settings["uci-settings"])
+
+        # Create a round instance and expose the bindings
         round_instance: Round = Round(chess_board=chess_board, transport=transport, chess_engine=engine)
+        await round_instance.create_shadow_root(page=page)
 
         # Configure the engine limits
         round_instance.chess_engine_limits.depth = settings["engine-limits"]["depth"]
@@ -106,7 +111,57 @@ class Round:
         # Clear the takeback offer origin variable
         self._takeback_offer_origin = ""
 
+    async def create_shadow_root(self, page: Page) -> None:
+        # Create a shadow-root in the board element and add a resize observer
+        board_locator: Locator = page.locator(selector="cg-board")
+        self.shadow_root = await board_locator.evaluate_handle(
+            expression="""boardElement => {
+                // Create a closed shadow-root
+                const shadowRoot = boardElement.attachShadow({mode: "closed"});
+                shadowRoot.innerHTML = `
+                    <canvas id="drawing-canvas" style="
+                        position: relative;
+                        z-index: 3;
+                        pointer-events: none;"></canvas>
+                    <slot></slot>`;
+
+                // Fetch the canvas element
+                const canvasElement = shadowRoot.querySelector("canvas");
+                canvasElement.width = boardElement.clientWidth;
+                canvasElement.height = boardElement.clientHeight;
+
+                // Attach a resize observer to the board element
+                new ResizeObserver(entries => entries.forEach(entry => {
+                    // Set the new size
+                    const contentRect = entry.contentRect;
+                    canvasElement.width = contentRect.width;
+                    canvasElement.height = contentRect.height;
+
+                    // Call the redraw_canvas binding
+                    window.redraw_canvas();
+                })).observe(boardElement);
+                // Return the shadow-root for later access
+                return shadowRoot;
+            }""".strip())
+
+    async def redraw_canvas(self) -> None:
+        await self.shadow_root.as_element().eval_on_selector(
+            selector="#drawing-canvas", expression="""canvas => {
+                const context2d = canvas.getContext("2d");
+                // Clear the canvas
+                context2d.clearRect(0, 0, canvas.width, canvas.height);
+
+                // Draw
+                context2d.lineWidth = 1;
+                context2d.beginPath();
+                context2d.moveTo(0, canvas.height / 8);
+                context2d.lineTo(canvas.width, canvas.height / 8);
+                context2d.closePath();
+                context2d.stroke();
+            }""".strip())
+
     async def shutdown(self) -> None:
+        # Note: do not call self.shadow_root.dispose(), it'll prevent Playwright from closing
         await self.chess_engine.quit()
         self.transport.close()
         await asyncio.sleep(0)
@@ -144,10 +199,12 @@ class Lichess(BrowserHandler):
         await self.browser.close()
 
     async def on_page(self, page: Page) -> None:
-        # Expose a binding to get_best_move
+        # Expose engine bindings
         await page.expose_binding("get_best_move", lambda source, depth=0: self.get_best_move(source["page"], depth))
         await page.expose_binding("set_depth", lambda source, depth: self.set_depth(source["page"], depth))
         await page.expose_binding("display_board", lambda source: self.display_board(source["page"]))
+        # Expose canvas bindings
+        await page.expose_binding("redraw_canvas", lambda source: self.redraw_canvas(source["page"]))
         # Register an event listener for websocket events
         page.on("websocket", functools.partial(self.on_websocket_created, page=page))
 
@@ -186,7 +243,7 @@ class Lichess(BrowserHandler):
 
         # Add a new round instance
         move_data: list[dict] = game_data.get("steps", game_data.get("treeParts", []))
-        self.chess_rounds[web_socket.url] = await Round.create(move_data=move_data)
+        self.chess_rounds[web_socket.url] = await Round.create(page=page, move_data=move_data)
 
         # Register web socket events
         web_socket.on("framereceived", functools.partial(self.on_websocket_message,
@@ -278,6 +335,14 @@ class Lichess(BrowserHandler):
             # Set the depth
             return str(chess_round.chess_board)
         return ""
+
+    async def redraw_canvas(self, page: Page) -> None:
+        round_identifier: str = page.url[page.url.rfind("/") + 1:]
+        for web_socket_url, chess_round in self.chess_rounds.items():
+            if round_identifier not in web_socket_url:
+                continue
+            await chess_round.redraw_canvas()
+            return
 
     async def perform_round_cleanup(self, socket_identifier: str) -> None:
         # Check if the game has already ended
