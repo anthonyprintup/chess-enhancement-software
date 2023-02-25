347c133fcfe0046144e38b7db9cffc708a4287a9
Anthony Printup <92564080+anthonyprintup@users.noreply.github.com>
2023-02-25 09:42:05 +0100

    fix(scripts): Check for existing shadow DOMs when creating one

--- src/scripts/create-shadow-root.js
+++ src/scripts/create-shadow-root.js
@@ -1,13 +1,23 @@
 // noinspection BadExpressionStatementJS
 (boardElement, heightOffset) => {
     // Create a closed shadow-root
-    const shadowRoot = boardElement.attachShadow({mode: "closed"});
+    let shadowRoot;
+    let attachResizeObserver = true;
+    try {
+        shadowRoot = boardElement.attachShadow({mode: "closed"});
+    } catch (exception) {
+        // Shadow root already exists (possible reconnection)
+        shadowRoot = boardElement._shadowRoot
+        attachResizeObserver = false;
+    }
+
+    boardElement._shadowRoot = shadowRoot;
     shadowRoot.innerHTML = `
-        <canvas id="drawing-canvas" style="
-            position: relative;
-            z-index: 3;
-            pointer-events: none;"></canvas>
-        <slot></slot>`;
+    <canvas id="drawing-canvas" style="
+        position: relative;
+        z-index: 3;
+        pointer-events: none;"></canvas>
+    <slot></slot>`;
 
     // Fetch the canvas element
     const canvasElement = shadowRoot.querySelector("canvas");
@@ -16,16 +26,17 @@
     canvasElement.height = boardElement.clientHeight + heightOffset;
 
     // Attach a resize observer to the board element
-    new ResizeObserver(entries => entries.forEach(entry => {
-        // Set the new size
-        const contentRect = entry.contentRect;
-        canvasElement.width = contentRect.width;
-        canvasElement.height = contentRect.height + heightOffset;
+    if (attachResizeObserver)
+        new ResizeObserver(entries => entries.forEach(entry => {
+            // Set the new size
+            const contentRect = entry.contentRect;
+            canvasElement.width = contentRect.width;
+            canvasElement.height = contentRect.height + heightOffset;
 
-        // Redraw any existing engine analysis (argument 1 is the new size of an individual piece)
-        // noinspection JSUnresolvedFunction
-        window.redraw_existing_engine_analysis(canvasElement.width);
-    })).observe(boardElement);
+            // Redraw any existing engine analysis (argument 1 is the new size of an individual piece)
+            // noinspection JSUnresolvedFunction
+            window.redraw_existing_engine_analysis(canvasElement.width);
+        })).observe(boardElement);
     // Return the shadow-root for later access
     return shadowRoot;
 };
