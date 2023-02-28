d77d93a48886ed88794ab301767e7a04a9713553
Anthony Printup <92564080+anthonyprintup@users.noreply.github.com>
2023-02-28 15:31:03 +0100

    refactor(Round): Refactored `execute_engine_control_schemes`
    Replaced the ply number with the current move number, added an indicator to see if the current move is to be played by the player or the opponent.

--- src/main.py
+++ src/main.py
@@ -195,13 +195,18 @@ class Round:
         self.execute_engine_control_schemes()
 
     def execute_engine_control_schemes(self) -> None:
-        ply_number: int = self.chess_board.ply()
+        move_number: int = self.chess_board.ply() // 2 + 1
         max_cp_score: int = max(self._cp_scores, default=(0, 0), key=lambda data: data[1])[1]
         engine_depth: int = self.chess_engine_limits.depth
 
         for engine_control_scheme in self.engine_control_schemes:
             # Declare the locals
-            locals_scope: dict = {"ply_number": ply_number, "max_cp_score": max_cp_score, "depth": engine_depth}
+            locals_scope: dict = {
+                "player_move": self.chess_board.turn == self.player_color,
+                "move_number": move_number,
+                "max_cp_score": max_cp_score,
+                "depth": engine_depth
+            }
             # Execute the control scheme (note: this is unsafe, even without builtins)
             exec(f"if {engine_control_scheme.condition}: {engine_control_scheme.operation}", {"__builtins__": None},
                  locals_scope)
